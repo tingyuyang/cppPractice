b24cd0dc4c6e9e5013932e96971469a784722d3c
Tingyu Yang <tingyuyang@lclark.edu>
2016-07-30 00:23:00 -0700

    Create function.cpp

---
+++ Project_5/function.cpp
@@ -0,0 +1,165 @@
+ //build up all the functions here
+
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <fstream>
+
+#include "class.h"
+
+Activity::Activity()//the construtoir to initialize every variable
+{
+   type = NULL;
+   location = NULL;
+   season = NULL;
+   info = NULL;
+   date =NULL;  
+}
+
+Activity::~Activity()//the destructor
+{
+   if (type != NULL)
+      delete []type;
+   if (location != NULL)
+      delete []location;
+   if (season != NULL)
+      delete []season;
+   if (info != NULL)
+      delete []info;
+   if (date != NULL)
+      delete []date;
+}
+void Activity::add_info()//the function for user to input the new information..
+{
+   int len = 0;// to count how many letters in the input word
+   char temptype[50];//temporary variable to string copy the user input//for input type
+   cout<<"Please type the name of the activity: ";
+   cin.get(temptype,50,'\n');//get the user input
+   cin.ignore(100,'\n');
+   len = strlen(temptype);//get the number of letters in the words from user input
+   type = new char[len+1];
+   strcpy(type,temptype);//to copy the input from temp to the type variable under Activity class
+
+   char temploc[50];//for location
+   cout<<"Please type the locatiin of the activity: ";
+   cin.get(temploc,50,'\n');
+   cin.ignore(100,'\n');
+   len = strlen(temploc);
+   location = new char[len+1];
+   strcpy(location,temploc);
+
+   char tempseason[50];//for activity season
+   cout<<"Please type the season of the activity: ";
+   cin.get(tempseason,50,'\n');
+   cin.ignore(100,'\n');
+   len = strlen(tempseason);
+   season = new char[len+1];
+   strcpy(season,tempseason);
+
+   char tempinfo[50];//for the user description of the activity
+   cout<<"Please type the information of the activity: ";
+   cin.get(tempinfo,50,'\n');
+   cin.ignore(100,'\n');
+   len = strlen(tempinfo);
+   info = new char[len+1];
+   strcpy(info,tempinfo);
+
+   char tempdate[50];//for the user to input the date of the activity
+   cout<<"Please enter the last time you did this activity(format: 7/13/2015): ";
+   cin.get(tempdate,50,'\n');
+   cin.ignore(100,'\n');
+   len =strlen(tempdate);
+   date= new char[len+1];
+   strcpy(date, tempdate);
+}
+
+//display all///only info so far
+void Activity::displayall()//to display the information of the activity
+{
+   cout<<"The activity name is "<<type<<" in "<<location<<" during season "<<season<<", the user description is "<<info <<endl;
+}
+
+//for season comparing,display the activity info
+void Activity::displayseason(char inseason[])//function to display if the season matches user input
+{
+   if (strcmp(inseason,season)==0)//so if the string compare matches user input, will apply the display function.
+      displayall();
+
+
+}
+
+/////////////////ADVENTURE
+//constructor
+Adventure::Adventure()//constructor to initialize
+{ 
+   head = NULL;
+}
+
+//////////////////for LLL
+//for inserting node into the LLL
+void Adventure::insert()//sth here?
+{
+   node * current = head;//so have a node named current which points to head
+
+   if (!head)// if the list is empty
+   {
+      head = new node;//have a new node
+      head->next=NULL;//so the node after head will be null
+      head ->sports.add_info();//then have the add info function from the activity
+      return;
+   }
+
+   while(current ->next)//to check if its the end of the list
+   {
+      current =current ->next;
+   }
+   
+   current ->next = new node;//the next node will be new
+   current ->next ->sports.add_info();//add info to the next node.
+   current ->next ->next =NULL;//set up the next to null.
+
+}
+
+//to display the information from the list
+void Adventure::displaylll()//what is here?
+{
+   if (!head)//if there is no list
+      return;
+
+    node *temp = head;//have a temp node point to head now
+
+    do
+   {
+      temp->sports.displayall();//display the informtaion
+      temp = temp ->next;//then point to next one
+   }while(temp!= NULL);//loop will stop till its the end of the list
+}
+
+
+//display all the matches seasons,
+void Adventure::displayseasonlll(char season[])
+{
+   node *temp = head;//have a temp node points to head
+
+   do
+   {
+      temp->sports.displayseason(season);//so display all the activities matched with the season
+      temp=temp->next;
+   }while (temp!=NULL);//loop will stop if  its the end of the list
+
+
+}
+
+
+//deconstructor//LLL deconstrutor
+Adventure::~Adventure()//delete the list
+{
+      node * temp = head;
+      while (head)
+      {
+	 temp=temp->next;
+	 delete head;
+	 head=temp;
+      }
+}
+
