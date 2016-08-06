efd4d31d3e9b1ef98d297de95004ef5f83a30f69
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-06 00:23:45 -0700

    Create cs162_list.cpp

---
+++ Lab_9/cs162_list.cpp
@@ -0,0 +1,57 @@
+#include "cs162_list.h"
+
+
+//These are the functions to implement for this lab
+//Notice that they are not in a class this time!
+//This means we need to pass in head as an argument
+//since there is no "data member" shared
+
+
+//Check to see if the desired item is in the list
+//Return true if it is found
+bool find(node * head, int match) 
+{
+    //Step 2 - Place your code here
+    if (head == NULL)
+    {
+        return false;
+    }
+    
+    node * current=head;
+    while (current)
+    {
+        if (current->data == match)
+        {
+            return true;
+        }
+       
+        current = current -> next;
+    }
+    return false;
+}
+
+//Remove all nodes in the list
+void remove_all(node * & head) 
+{
+      //Step 3 - Place your code here
+      if (!head)
+          return;
+      remove_all(head ->next);
+      delete head;
+      head = NULL;
+}
+
+
+//Display every other item starting with the first
+void display_every_other(node * head) 
+{
+      //Step 4 - Place your code here
+      if (!head)
+          return;
+      
+      cout<<head->data<<endl;
+      if (head ->next)
+          display_every_other(head ->next->next);
+      
+}
+
