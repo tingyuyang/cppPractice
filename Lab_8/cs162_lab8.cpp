42c29537b8ca3d1d9468c7ce65adf7a00096e856
Tingyu Yang <tingyuyang@lclark.edu>
2016-08-06 00:28:36 -0700

    Create cs162_lab8.cpp

---
+++ Lab_8/cs162_lab8.cpp
@@ -0,0 +1,27 @@
+#include "cs162_list.h"
+
+
+int main()
+{
+    list my_values;
+    
+    my_values.build();
+          
+
+    //PUT YOUR FUNCTION CALL HERE!
+
+    my_values.insert(5);
+    my_values.append(7);
+
+    int y;
+    y=my_values.count();
+    cout<<"Yeah,you have "<<y<<" in your LLL"<<endl;
+
+    my_values.remove_last();
+    my_values.~list();
+
+    my_values.display_all();
+     
+    return 0;
+
+}
